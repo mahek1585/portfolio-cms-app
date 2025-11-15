5aae6494e067dfd10ccc790ac7c45cf6c888fb61
Mahek9405 <mahekshaikh9405@gmail.com>
2025-11-15 11:29:56 +0530

    feat(JourneyHero): created Journey hero section component

--- frontend/src/portfolio/pages/Journey/Journey.jsx
+++ frontend/src/portfolio/pages/Journey/Journey.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Header from '../../components/layout-ui/Header'
+import JourneyHero from './JourneyHero'
 
 const Journey = () => {
   return (
     <div>
         <Header />
-        
+        <JourneyHero />
          
         
     </div>
